70f0493075cf7e80c278b7ec30a5113a2e1a48bd
taind <nguyenductai.job@gmail.com>
2020-02-01 00:41:40 +0700

    Added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftyLib.swift
+//
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SwiftyLibTests/SwiftyLibTests.swift
+++ SwiftyLibTests/SwiftyLibTests.swift
@@ -11,8 +11,11 @@ import XCTest
 
 class SwiftyLibTests: XCTestCase {
 
+    var swiftyLib: SwiftyLib!
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyLib = SwiftyLib()
+
     }
 
     override func tearDown() {
@@ -29,6 +32,13 @@ class SwiftyLibTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+    }
+    func testSub() {
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
     }
 
 }
